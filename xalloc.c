1d6e15c85d5914e867fe35c7c75e7c3f70745ca5
Andreas Gruenbacher <agruen@linbit.com>
2013-10-04 17:51:04 +0200

    Improve the copyright and license information

--- xalloc.c
+++ xalloc.c
@@ -1,3 +1,16 @@
+/*
+   Author: Andreas Gruenbacher <agruen@linbit.com>
+
+   Copyright (C) 2013 LINBIT HA-Solutions GmbH, http://www.linbit.com
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   See the COPYING file for details.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include "xalloc.h"
